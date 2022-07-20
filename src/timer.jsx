c65de1adf14b832a63e0afb43599a4d70c8e3554
Tom Odell <tom.odell@junipereducation.org>
2022-07-20 16:40:28 +0100

    Add timer

---
+++ src/timer.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const Timer = () => {
+  const startTime = 5;
+  const [timer, setTimer] = useState(startTime);
+
+  useEffect(() => {
+    const runningTimer = setTimeout(() => {
+      if(timer > 0) {
+        setTimer(timer-1);
+      }
+    }, 1000);
+
+    return clearTimeout(runningTimer);
+  }, [timer])
+  
+  return (
+    <>
+      {timer}
+      <br />
+      <button onClick={() => {setTimer(startTime)}}>Reset</button>
+    </>
+  )
+}
+
+export default Timer;
